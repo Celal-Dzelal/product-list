9a4937e11cf57cdb2891adf3618c9031616c2262
Celal <binaycelalselim@gmail.com>
2024-12-30 03:18:05 +0300

    Update All

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import ProductCard from "./components/ProductCard/ProductCard";
+import { products, categories } from "./helper/data";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header categories={categories} title="PRODUCTS LIST" />
+      <ProductCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ProductStyle from "./ProductCard.module.scss";
+
+function ProductCard() {
+  return <div className={ProductStyle.gallery}>ProductCard</div>;
+}
+
+export default ProductCard;
